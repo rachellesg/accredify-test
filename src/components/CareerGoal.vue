dc1bd05733009c25e13804867a3522bb11478eeb
Rachelle <rachelle.yeo@outlook.com>
2023-06-11 14:06:39 +0800

    update: career goal style

--- src/components/CareerGoal.vue
+++ src/components/CareerGoal.vue
@@ -9,12 +9,24 @@ defineProps<{
     <div class="row">
       <h4>{{ title }}</h4>
     </div>
-    <div class="container">Etc etc</div>
+    <div class="container">
+      <h6>Your Progress</h6>
+      I want to become a
+      <h4>Tax Manager</h4>
+      <RouterLink to="/">View Insights</RouterLink>
+    </div>
   </section>
 </template>
 
 <style scoped>
 section {
   width: 25%;
+}
+
+.container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 }
 </style>
