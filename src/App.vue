f73e1cc7f7666f75e32e1961428050e87858d09c
Rachelle <rachelle.yeo@outlook.com>
2023-06-12 19:21:38 +0800

    update menu links

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,41 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+
+const menuLinks = [
+  {
+    name: 'Dashboard Homepage',
+    link: '/',
+    icon: '/src/assets/icons/home.svg'
+  },
+  {
+    name: 'Documents',
+    link: '/',
+    icon: '/src/assets/icons/file.svg'
+  },
+  {
+    name: 'Some Page',
+    link: '/',
+    icon: '/src/assets/icons/bulb.svg'
+  },
+  {
+    name: 'Another Page',
+    link: '/',
+    icon: '/src/assets/icons/shield.svg'
+  },
+  {
+    name: 'Settings',
+    link: '/',
+    icon: '/src/assets/icons/setting.svg'
+  }
+]
 </script>
 
 <template>
   <div class="menu">
     <img alt="logo" class="logo" src="@/assets/logo.svg" width="40" height="40" />
     <nav>
-      <RouterLink to="/">
-        <img alt="Dashboard Homepage" src="@/assets/icons/home.svg" width="18" height="18" />
-      </RouterLink>
-      <RouterLink to="/">
-        <img alt="Documents" src="@/assets/icons/file.svg" width="18" height="18" />
-      </RouterLink>
-      <RouterLink to="/">
-        <img alt="Some page" src="@/assets/icons/bulb.svg" width="18" height="18" />
-      </RouterLink>
-      <RouterLink to="/">
-        <img alt="Another page" src="@/assets/icons/shield.svg" width="18" height="18" />
-      </RouterLink>
-      <RouterLink to="/">
-        <img alt="Settings" src="@/assets/icons/setting.svg" width="18" height="18" />
+      <RouterLink v-for="link in menuLinks" :to="link.link" :key="link.name">
+        <img :alt="link.name" :src="link.icon" />
       </RouterLink>
     </nav>
   </div>
@@ -52,14 +68,6 @@ nav {
   display: flex;
   flex-direction: column;
   align-items: flex-start;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
 }
 
 nav a {
