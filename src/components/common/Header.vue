cba3d079c66a3bb63913cba53fa26dc92516062a
Rachelle <rachelle.yeo@outlook.com>
2023-06-12 09:56:09 +0800

    feat: extract top bar as component

---
+++ src/components/common/Header.vue
@@ -0,0 +1,55 @@
+<script lang="ts">
+export default {
+  props: {
+    profilePictureUrl: {
+      type: String
+    },
+    name: {
+      type: String
+    }
+  }
+}
+</script>
+
+<template>
+  <header>
+    <div />
+    <div class="content settings">
+      <img :src="profilePictureUrl" class="profile-photo" alt="Profile Picture" />
+      {{ name }}
+
+      <img src="@/assets/icons/chevron-down.svg" />
+    </div>
+  </header>
+</template>
+
+<style scoped>
+header {
+  height: 64px;
+  position: relative;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+}
+
+header:after {
+  content: '';
+  height: 1px;
+  width: 100%;
+  background: #d0d2d6;
+  position: absolute;
+  bottom: 0;
+}
+.settings {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  width: 300px;
+}
+.profile-photo {
+  width: 20px;
+  height: 20px;
+  border-radius: 10px;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -17,6 +17,7 @@ onMounted(fetchUser)
 <script lang="ts">
 import CareerGoal from '@/components/CareerGoal.vue'
 import RecentDocument from '@/components/RecentDocument.vue'
+import Header from '@/components/common/Header.vue'
 
 const userData = ref<User | null>(null)
 
@@ -38,25 +39,8 @@ interface User {
 
 <template>
   <main>
-    <header>
-      <div />
-      <div class="content settings">
-        <img :src="userData?.profile_picture_url ?? ''" alt="Profile Picture" />
-        {{ userData?.name }}
-        <svg
-          width="10"
-          height="6"
-          viewBox="0 0 10 6"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M9.28255 0.255061C8.98592 -0.0850202 8.51978 -0.0850202 8.22314 0.255061L5.00252 3.94737L1.78189 0.255061C1.48526 -0.0850202 1.01911 -0.0850202 0.722477 0.255061C0.425841 0.595142 0.425841 1.12955 0.722477 1.46964L4.45162 5.74494C4.74826 6.08502 5.2144 6.08502 5.51104 5.74494L9.24018 1.46964C9.57919 1.12955 9.57919 0.595142 9.28255 0.255061Z"
-            fill="#151F32"
-          />
-        </svg>
-      </div>
-    </header>
+    <Header :profilePictureUrl="userData?.profile_picture_url" :name="userData?.name" />
+
     <div class="content">
       <section>
         <h1>Hi, {{ userData?.name }} 👋</h1>
@@ -66,10 +50,10 @@ interface User {
       </section>
       <div class="wrapper">
         <!-- <component
-        v-if="user && user.current_organisation.is_personal"
-        :is="CareerGoal"
-        :title="Career Goal"
-      /> -->
+          v-if="userData && userData?.current_organisation?.is_personal"
+          :is="CareerGoal"
+          title="Career Goal"
+        /> -->
 
         <CareerGoal title="Career Goal" />
         <RecentDocument title="Recent Document" />
@@ -84,34 +68,6 @@ main {
   min-height: 100vh;
   background: white;
   border-top-left-radius: 16px;
-}
-
-header {
-  height: 64px;
-  position: relative;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  width: 100%;
-}
-
-header:after {
-  content: '';
-  height: 1px;
-  width: 100%;
-  background: #d0d2d6;
-  position: absolute;
-  bottom: 0;
-}
-.settings {
-  display: flex;
-  justify-content: flex-end;
-  align-items: center;
-}
-.settings img {
-  width: 20px;
-  height: 20px;
-  border-radius: 10px;
 }
 
 @media screen and (max-width: 768px) {
