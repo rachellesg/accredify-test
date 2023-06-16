bad8f6a521089d3b37f489aab70dee4a1936e896
Rachelle <rachelle.yeo@outlook.com>
2023-06-16 08:47:38 +0800

    update for GH pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,18 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath } from 'url';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueJsx from '@vitejs/plugin-vue-jsx';
+import path from 'path';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+});
